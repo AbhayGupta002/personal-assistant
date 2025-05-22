5f079bde841407b3966f621141ec15af74c4cc90
Abhay Gupta <ag4035737@gmail.com>
2025-05-23 00:29:13 +0530

    Update TaskReminderRepository.java

--- src/main/java/com/personal/assistant/repository/TaskReminderRepository.java
+++ src/main/java/com/personal/assistant/repository/TaskReminderRepository.java
@@ -10,17 +10,25 @@ import org.springframework.data.repository.query.Param;
 
 import com.personal.assistant.entity.TaskReminder;
 
+import java.util.Optional;
+
 public interface TaskReminderRepository extends JpaRepository <TaskReminder , String> {
     @Query(
-        value = "SELECT * FROM task_reminder t WHERE t.user_id = :userId AND t.task_name = '%:taskName%' ORDER BY t.time ASC LIMIT 1",
+        value = "SELECT * FROM task_reminder t WHERE t.user_id = :userId AND t.task_name = '%:taskName%' ORDER BY t.reminder_time ASC LIMIT 1",
         nativeQuery = true
     )
-    TaskReminder findByName (@Param("taskName") String taskName, @Param("userId") String userId);
+    Optional<TaskReminder> findByName (@Param("taskName") String taskName, @Param("userId") String userId);
 
     @Query(
-        value = "SELECT * FROM task_reminder t WHERE t.user_id = :userId AND t.time > NOW() ORDER BY t.reminder_time ASC LIMIT 1",
+        value = "SELECT * FROM task_reminder t WHERE t.user_id = :userId AND t.reminder_time > NOW() ORDER BY t.reminder_time ASC LIMIT 1",
         nativeQuery = true
     )
-    TaskReminder findNextActiveTask (@Param("userId") String userId);
+    Optional<TaskReminder> findNextActiveTask (@Param("userId") String userId);
+
+    @Query(
+        value = "SELECT * FROM task_reminder t WHERE t.task_id = :taskId AND t.reminder_time > NOW() ORDER BY t.reminder_time ASC LIMIT 1",
+        nativeQuery = true
+    )
+    Optional<TaskReminder> findByTaskId (@Param("taskId") String task_id);
 
 }
