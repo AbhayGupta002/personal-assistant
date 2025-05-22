920161d6883c855df95718aed2962e1bbbb89175
Abhay Gupta <ag4035737@gmail.com>
2025-05-23 00:28:37 +0530

    Update TaskDetailsRepository.java

--- src/main/java/com/personal/assistant/repository/TaskDetailsRepository.java
+++ src/main/java/com/personal/assistant/repository/TaskDetailsRepository.java
@@ -1,16 +1,19 @@
 package com.personal.assistant.repository;
 
+import com.personal.assistant.dto.TaskDto;
 import com.personal.assistant.entity.TaskDetails;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
+
 public interface TaskDetailsRepository extends JpaRepository<TaskDetails, String> {
 
     @Query(
-        value = "SELECT * FROM task_details t WHERE t.user_id = :userId AND t.task_id = :taskId ORDER BY t.time ASC LIMIT 1",
+        value = "SELECT * FROM task_details WHERE user_id = :userId AND task_id = :taskId",
         nativeQuery = true
     )
-    TaskDetails findByTaskAndUserId(@Param("taskId") String taskId, @Param("userId") String userId);
+    Optional<TaskDetails> findByTaskAndUserId(@Param("taskId") String taskId, @Param("userId") String userId);
 
 }
