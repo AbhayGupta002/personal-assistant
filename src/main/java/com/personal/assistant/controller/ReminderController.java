936e40c9ce594095ede4c36fb0490aeceb7010e5
Abhay Gupta <ag4035737@gmail.com>
2025-06-14 21:22:26 +0530

    Update ReminderController.java

--- src/main/java/com/personal/assistant/controller/ReminderController.java
+++ src/main/java/com/personal/assistant/controller/ReminderController.java
@@ -1,4 +1,5 @@
 package com.personal.assistant.controller;
+
 import com.personal.assistant.dto.TaskDto;
 import com.personal.assistant.entity.TaskDetails;
 import com.personal.assistant.entity.TaskReminder;
@@ -7,18 +8,14 @@ import com.personal.assistant.enums.Status;
 import com.personal.assistant.repository.TaskDetailsRepository;
 import com.personal.assistant.repository.TaskReminderRepository;
 import com.personal.assistant.repository.UserDetailsRepository;
-
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
-
-
 import org.springframework.web.bind.annotation.*;
 
 
 @RestController
 @RequestMapping("reminder")
 public class ReminderController {
+
     @Autowired
     private TaskDetailsRepository taskRepo;
 
@@ -31,8 +28,10 @@ public class ReminderController {
     @PostMapping("/")
     public TaskDto getReminder(@RequestBody String userId) {
         UserDetails user = userRepo.findById(userId).orElseThrow(() -> new RuntimeException("User not found for this id :: " + userId));
-        TaskReminder taskReminder = reminderRepo.findNextActiveTask(user.getId());
-        TaskDetails taskDetails = Optional.of(taskRepo.findByTaskAndUserId(taskReminder.getTaskId(), user.getId())).orElseThrow(() -> new RuntimeException("Task not found for this id :: " + taskReminder.getTaskId()));
+        TaskReminder taskReminder = reminderRepo.findNextActiveTask(user.getId())
+                .orElseThrow(() -> new RuntimeException("Task Reminder not found for this id :: " + userId));
+        TaskDetails taskDetails = taskRepo.findByTaskAndUserId(taskReminder.getTaskDetails().getTaskId(), user.getId())
+                .orElseThrow(() -> new RuntimeException("Task not found for this id :: " + taskReminder.getTaskDetails().getTaskId()));
 
         TaskDto task = new TaskDto();
         task.setTaskId(taskDetails.getTaskId());
@@ -46,52 +45,5 @@ public class ReminderController {
 
         return task;
     }
-    
-     @GetMapping(value = "get")
-    public TaskDto getTask(@RequestBody TaskDto taskDto){
-        TaskDetails taskDetails = taskRepo.findByTaskAndUserId(taskDto.getTaskId(), taskDto.getUserId())
-                .orElseThrow(() -> new RuntimeException("task not existing"+taskDto.getTaskId()));
-        TaskReminder taskReminder = reminderRepo.findByTaskId(taskDto.getTaskId())
-                .orElseThrow(() -> new RuntimeException("task not existing"+taskDto.getTaskId()));
-        TaskDto task = new TaskDto();
-        task.setTaskId(taskDetails.getTaskId());
-        task.setUserId(taskDetails.getUserId());
-        task.setTaskName(taskDetails.getTaskName());
-        task.setDateTime(taskDetails.getDateTime());
-        task.setCreatedAt(taskDetails.getCreatedAt());
-        task.setTaskCompleted(taskDetails.isTaskCompleted());
-        task.setTaskStatus(Status.valueOf(taskDetails.getTaskStatus()));
-        taskReminder.setReminderTime(taskReminder.getReminderTime());
-        return task;
 
-    }
-
-    @PutMapping(value = "update")
-    public TaskDto updateTask(@RequestBody TaskDto taskDto) {
-        TaskDetails taskDetails = taskRepo.findByTaskAndUserId(taskDto.getTaskId(), taskDto.getUserId())
-                .orElseThrow(() -> new RuntimeException("task not existing"+taskDto.getTaskId()));
-        TaskReminder taskReminder = reminderRepo.findByTaskId(taskDto.getTaskId())
-                .orElseThrow(() -> new RuntimeException("task not existing"+taskDto.getTaskId()));
-        taskDetails.setTaskName(taskDto.getTaskName());
-        taskDetails.setDateTime(taskDto.getDateTime());
-        taskDetails.setTaskStatus(Status.ACTIVE.name());
-        taskDetails.setCreatedAt(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
-        taskDetails.setTaskCompleted(false);
-        TaskDetails savedTask = taskRepo.save(taskDetails);
-
-        taskReminder.setReminderTime(taskDto.getReminderTime());
-        taskReminder.setTaskId(savedTask.getTaskId());
-        TaskReminder savedReminder = reminderRepo.save(taskReminder);
-
-        taskDto.setTaskId(savedTask.getTaskId());
-        taskDto.setTaskStatus(Status.valueOf(savedTask.getTaskStatus()));
-        taskDto.setCreatedAt(savedTask.getCreatedAt());
-        taskDto.setTaskCompleted(savedTask.isTaskCompleted());
-        taskDto.setReminderTime(savedReminder.getReminderTime());
-
-        return taskDto;
-
-}
-
-    
 }
