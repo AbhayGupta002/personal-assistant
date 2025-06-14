80da3df95317d66a66e0b13faebd002a921030ae
Abhay Gupta <ag4035737@gmail.com>
2025-06-14 21:20:48 +0530

    Create Response.java

---
+++ src/main/java/com/personal/assistant/dto/Response.java
@@ -0,0 +1,13 @@
+package com.personal.assistant.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Response {
+    Object data;
+    ErrorDetails error;
+}
