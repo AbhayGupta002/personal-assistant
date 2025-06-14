4cfefad0889247591dd48a2471687f93b4e32ef0
Abhay Gupta <ag4035737@gmail.com>
2025-06-14 21:22:03 +0530

    Update LoginController.java

--- src/main/java/com/personal/assistant/controller/LoginController.java
+++ src/main/java/com/personal/assistant/controller/LoginController.java
@@ -1,14 +1,14 @@
 package com.personal.assistant.controller;
 
+import com.personal.assistant.dto.ErrorDetails;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-
 import com.personal.assistant.dto.UserDto;
 import com.personal.assistant.entity.UserDetails;
 import com.personal.assistant.entity.UserLogin;
 import com.personal.assistant.repository.UserDetailsRepository;
 import com.personal.assistant.repository.UserLoginRepository;
-
 import java.util.UUID;
 
 @RestController
@@ -31,12 +31,21 @@ public class LoginController {
         userDetails.setContactNumber(user.getContactNumber());
         userDetails.setEmail(user.getEmail());
         userDetails.setStatus("Active");
-        userRepo.save(userDetails);
+        try{
+            userRepo.save(userDetails);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
 
         UserLogin login = new UserLogin();
         login.setEmail(user.getEmail());
         login.setPassword(user.getPassword());
-        loginRepo.save(login);
+        try{
+            loginRepo.save(login);
+        } catch (Exception e) {
+            ErrorDetails errorDetails = new ErrorDetails(HttpStatus.INTERNAL_SERVER_ERROR,
+                    "Email id or password not saved");
+        }
 
         UserDto dto = new UserDto();
         dto.setId(userDetails.getId());
@@ -44,14 +53,15 @@ public class LoginController {
         dto.setAddress(userDetails.getAddress());
         dto.setEmail(userDetails.getEmail());
         dto.setContactNumber(userDetails.getContactNumber());
-        
+
         return dto;
     }
 
     @GetMapping(value = "/")
     public UserDto getUser (@RequestBody UserDto userDto) {
-        UserDetails userDetails = userRepo.findById(userDto.getId()).orElseThrow(() -> new RuntimeException("User not found with id " + userDto.getId()));
         UserDto user = new UserDto();
+        UserDetails userDetails = userRepo.findById(userDto.getId()).orElseThrow(() -> new RuntimeException("User not found with id " + userDto.getId()));
+
         user.setId(userDetails.getId());
         user.setName(userDetails.getName());
         user.setAddress(userDetails.getAddress());
@@ -60,7 +70,6 @@ public class LoginController {
         return user;
     }
 
-    // Login function - basic example
     @PostMapping("authenticate")
     public String userLogin(@RequestBody UserLogin login) {
         int retVal = loginRepo.findByEmailAndPassword(login.getEmail(), login.getPassword());
